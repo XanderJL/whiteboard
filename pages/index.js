0ac22c26b2c1c18c8926fff8d8ebd3a0369590e9
Alexander Low <alexanderlow@MacBook-Air.local>
2020-09-10 14:54:19 -0400

    refactored: index page component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
 import React from "react"
 import Head from "next/head"
+import { Flex, Heading } from "@chakra-ui/core"
 
 const Index = () => {
-  return <div></div>
+  return (
+    <>
+      <Head>
+        <title>Whiteboard</title>
+        <meta
+          name="description"
+          content="Temporary whiteboard app for Aluminum Associates"
+        />
+        <meta name="author" content="Alex Low" />
+      </Head>
+      <Flex direction="row" wrap="wrap" align="center" justify="space-around">
+        <Flex direction="column">
+          <Heading as="h1" size="xl">ALUMINUM</Heading>
+        </Flex>
+        <Flex direction="column">
+          <Heading as="h1" size="xl">VINYL</Heading>
+        </Flex>
+      </Flex>
+    </>
+  )
 }
 
 export default Index
