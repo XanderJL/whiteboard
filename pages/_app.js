de98f67ec4435d8edc688654749d9fa9b74df24b
XanderJL <alexanderjameslow@gmail.com>
2020-09-10 23:14:04 -0400

    added: providers for app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { CSSReset, theme, ThemeProvider } from "@chakra-ui/core"
+import { DragDropContext } from "react-beautiful-dnd"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <DragDropContext>
+    <ThemeProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </DragDropContext>
+  )
 }
 
 export default MyApp
