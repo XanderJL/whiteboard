ad3ccf31f3acd81c0907a1b621dd3ded973e1f3f
Alexander Low <alexanderlow@MacBook-Air.local>
2020-09-12 00:43:23 -0400

    added: styling w/ chakra components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Head from "next/head"
-import { Box, Flex, Heading } from "@chakra-ui/core"
+import { Box, Flex, Grid, Heading } from "@chakra-ui/core"
 import { Draggable, Droppable } from "react-beautiful-dnd"
 
 const Index = () => {
@@ -14,8 +14,16 @@ const Index = () => {
         />
         <meta name="author" content="Alex Low" />
       </Head>
-      <Box display="flex" flexDir="column" w="100%" h="100vh" p="3rem 1.25rem">
-        <Flex direction="row" wrap="wrap" align="center" justify="space-around">
+      <Box
+        display="flex"
+        flexDir="column"
+        w="100%"
+        h="100vh"
+        p={["1.25rem", "3rem 1.25rem"]}
+      >
+        <Grid
+          gridTemplateColumns={["minmax(0, 1fr)", "repeat(2, minmax(0, 1fr))"]}
+        >
           <Box>
             <Heading as="h1" size="xl">
               ALUMINUM
@@ -30,7 +38,7 @@ const Index = () => {
             </Heading>
             <Flex direction="column">also bleh</Flex>
           </Box>
-        </Flex>
+        </Grid>
       </Box>
     </>
   )
